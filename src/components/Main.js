ddf0f8cf75646d7c72b4b4ecd7afa21999bff636
lukitos <lukitos@yahoo.com>
2017-11-28 11:56:20 -0700

    @anthonykulis fixed table formatting

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
 
 const styles = theme => ({
   root: {
@@ -16,48 +17,37 @@ const styles = theme => ({
   },
 });
 
-// let id = 0;
-// function createData(name, calories, fat, carbs, protein) {
-//   id += 1;
-//   return { id, name, calories, fat, carbs, protein };
-// }
-//
-// const data = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
 const Main = (props) => {
   console.log('in Main >>> props', props);
 
   const { classes } = props;
 
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell numeric>Ranking</TableCell>
-            <TableCell>Angler</TableCell>
-            <TableCell numeric>Weight</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.fish.map((n, i) => {
-            return (
-              <TableRow key={i}>
-                <TableCell numeric>{i+1}</TableCell>
-                <TableCell>{n.angler}</TableCell>
-                <TableCell numeric>{n.weight}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
+    <div style={{ padding: 20 }}>
+      <Typography type="title" align="center">All Species</Typography>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell numeric>Ranking</TableCell>
+              <TableCell>Angler</TableCell>
+              <TableCell numeric>Weight</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.fish.map((n, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell numeric>{i+1}</TableCell>
+                  <TableCell>{n.angler}</TableCell>
+                  <TableCell numeric>{n.weight}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
   );
 
 }
